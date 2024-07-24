b1417e2963b07a4173139541f834741635f8fb34
Inscrutablydistinct <121133033+Inscrutablydistinct@users.noreply.github.com>
2024-07-24 13:15:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ publication_date
 arxiv_id
 results
 
-Note:
+PLease note that:
 - The attribute 'keywords' must be non-empty and always present in the final output.
 - The main query should be the input user query.
 - If a query specifies a date, include "<", ">", ">=", "<=", "=" to denote before, after, after and on, before and on, and on the publication date, respectively.
 - If the query includes a metadata attribute term (e.g., author) without a specific name, include it in the main query instead of identifying it as a metadata attribute.
 - To reinstate the previous point don't give those metadata attributes whose values are unknown or you are not confident about.
 - The 'abstract' attribute should be present in all, but it shouldn't be more than 20 words long.
-- Give the output at all costs, always include the main query and the non empty/null identified metadata attributes.
+- Give the output at all costs, always include the main query and the non empty identified metadata attributes.
 
 Examples:
 
