55049811377ef41d6568b760887497398a554fec
Inscrutablydistinct <121133033+Inscrutablydistinct@users.noreply.github.com>
2024-07-29 16:04:37 +0530

    Update filter_by_metadata.py

--- filter_by_metadata.py
+++ filter_by_metadata.py
@@ -1,75 +1,57 @@
 import warnings
-warnings.filterwarnings("ignore")
-from model_param import embeddings
-from sklearn.metrics.pairwise import cosine_similarity
-import transformers
-from transformers import (
-    AutoTokenizer, AutoModelForCausalLM,
-    BitsAndBytesConfig,
-    pipeline
-)
-from model_param import CFG, embeddings
-from langchain_community.vectorstores import FAISS
 from datetime import datetime
+from rank_bm25 import BM25Okapi
+from model_param import CFG
+from langchain_community.vectorstores import FAISS
 
-def compute_cosine_similarity(text1, text2):
-    embed1 = embeddings.embed_query(str(text1))
-    return cosine_similarity([embed1], [text2])[0][0]
+warnings.filterwarnings("ignore")
 
-def filter_attributes(metadata_entry, key, value):
-    if (key=='title'):
-        cos_sim = compute_cosine_similarity(metadata_entry['title'], value)
-        return cos_sim*5
-    elif (key == 'author'):
-        cos_sim = compute_cosine_similarity(metadata_entry['author'], value)
-        return cos_sim*5
-    elif (key == 'abstract'):
-        cos_sim = compute_cosine_similarity(metadata_entry['abstract'], value)
-        return cos_sim*5
-    elif (key == 'keywords'):
-        cos_sim = compute_cosine_similarity(metadata_entry['keywords'], value)
-        return cos_sim*5
-    elif (key == 'publication_date'):
+def tokenize(text):
+    return text.lower().split()
+
+def compute_bm25_score(corpus, query):
+    tokenized_corpus = [tokenize(doc) for doc in corpus]
+    bm25 = BM25Okapi(tokenized_corpus)
+    tokenized_query = tokenize(query)
+    scores = bm25.get_scores(tokenized_query)
+    return scores
+
+def filter_attributes(metadata_entry, key, value, corpus_store):
+    if key in ['title', 'author', 'abstract', 'keywords', 'results']:
+        field_text = metadata_entry.get(key, "")
+        corpus = corpus_store.get(key, [])
+        scores = compute_bm25_score(corpus, value)
+        index = corpus.index(field_text) if field_text in corpus else -1
+        return scores[index] * 5 if index != -1 else 0.0
+    elif key == 'publication_date':
         op = value[0] if value[1].isdigit() else value[0:2]
         value = value[len(op):]
         filter_date = datetime.strptime(value, "%Y-%m-%d")
         if metadata_entry['publication_date'] == "N/A":
             return 0.0
         entry_date = datetime.strptime(metadata_entry['publication_date'], "%Y-%m-%d")
-        if (op == '>'):
+        if op == '>':
             return 2.0 if entry_date > filter_date else -6.0
-        elif (op == '>='):
+        elif op == '>=':
             return 2.0 if entry_date >= filter_date else -6.0
-        elif (op == '<'):
+        elif op == '<':
             return 2.0 if entry_date < filter_date else -6.0
-        elif (op == '<='):
+        elif op == '<=':
             return 2.0 if entry_date <= filter_date else -6.0
         else:
             return 2.0 if entry_date == filter_date else -6.0
-    elif (key == 'results'):
-        if (type(metadata_entry['results'])==list):
-            metadata_entry['results'] = " ".join(metadata_entry['results'])
-        cos_sim = compute_cosine_similarity(metadata_entry['results'], value)
-        return cos_sim
     else:
         return 0.0
 
 def filter_data(metadata, filter_dict):
-    vectordb = FAISS.load_local(CFG.Output_folder + '/faiss_index_papers', # from output folder
-        embeddings,
-        allow_dangerous_deserialization = True,)
+    vectordb = FAISS.load_local(CFG.Output_folder + '/faiss_index_papers', embeddings, allow_dangerous_deserialization=True)
     scored_metadata = []
-    store = {}
+    corpus_store = {key: [entry.get(key, "") for entry in metadata] for key in filter_dict.keys() if key != 'publication_date'}
+
     for entry in metadata:
         total_score = 0.0
         for key, value in filter_dict.items():
-            if (key == 'publication_date'):
-                total_score += filter_attributes(entry, key, value)
-            elif key in store:
-                total_score += filter_attributes(entry, key, store[key])
-            else:
-                store[key] = embeddings.embed_query(value)
-                total_score += filter_attributes(entry, key, store[key])
+            total_score += filter_attributes(entry, key, value, corpus_store)
         print(total_score)
         scored_metadata.append((total_score, entry))
 
