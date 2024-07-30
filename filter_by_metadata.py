6cc483e910852929cc8a1b8cc120bf00d2b01dd2
Inscrutablydistinct <121133033+Inscrutablydistinct@users.noreply.github.com>
2024-07-30 11:37:50 +0530

    Update filter_by_metadata.py

--- filter_by_metadata.py
+++ filter_by_metadata.py
@@ -7,9 +7,9 @@ from langchain_community.vectorstores import FAISS
 warnings.filterwarnings("ignore")
 
 def tokenize(text):
+    print([a.lower.split() for a in text] if isinstance(text, list) else text.lower.split())
     return [a.lower.split() for a in text] if isinstance(text, list) else text.lower.split()
  
-
 def compute_bm25_score(corpus, query):
     # print(f"{corpus}\n")
     # print(query)
